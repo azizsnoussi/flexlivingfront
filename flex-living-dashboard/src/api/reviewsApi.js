d2ad006f12abfe3df984c8ce72aac944a0bc1cf9
mohamed aziz snoussi <63208771+azizsnoussi@users.noreply.github.com>
2025-08-12 00:51:13 +0100

    Update reviewsApi100.js
    test

--- flex-living-dashboard/src/api/reviewsApi.js
+++ flex-living-dashboard/src/api/reviewsApi.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 export async function fetchHostawayReviews() {
-  
   const response = await axios.get("https://flexliving.onrender.com/api/reviews/hostaway");
-  
   return response.data.reviews || [];
 }
