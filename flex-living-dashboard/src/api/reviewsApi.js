e24f8ecce4730a28899256f529ff0e1d4158af38
mohamed aziz snoussi <63208771+azizsnoussi@users.noreply.github.com>
2025-08-12 01:03:58 +0100

    fixing the review api.js
    fixx

--- flex-living-dashboard/src/api/reviewsApi.js
+++ flex-living-dashboard/src/api/reviewsApi.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export async function fetchHostawayReviews() {
   const response = await axios.get("https://flexliving.onrender.com/api/reviews/hostaway");
-  return response.data.reviews || [];
+  return response.data.reviews;
 }
